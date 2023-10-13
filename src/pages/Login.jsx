76ca6105518442aa9802751658b354b2a3712272
Anurag Mohanty <68963714+AR-Mohanty@users.noreply.github.com>
2023-10-13 11:14:30 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,10 +4,10 @@ import brandLogo from "../assets/brandLogo.png";
 
 const Login = () => {
   const google = () => {
-    window.open("http://localhost:5000/auth/google", "_self");
+    window.open("https://eapply-backend.vercel.app/auth/google", "_self");
   };
   const github = () => {
-    window.open("http://localhost:5000/auth/github", "_self");
+    window.open("https://eapply-backend.vercel.app/auth/github", "_self");
   };
   return (
     <section className="py-16">
